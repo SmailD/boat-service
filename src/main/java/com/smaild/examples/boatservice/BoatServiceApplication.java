51509462bc7e9e1644a0c234762b1c4f47e61274
DAOUDY Ismail <ismail.daoudy@gmail.com>
2020-05-02 13:03:38 +0200

    Init project

---
+++ src/main/java/com/smaild/examples/boatservice/BoatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smaild.examples.boatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatServiceApplication.class, args);
+	}
+
+}
